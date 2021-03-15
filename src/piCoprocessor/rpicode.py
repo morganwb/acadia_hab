96a076884eec54635523fe4fb3118bcc55aa1391
morganwb <morgan.baxter@blacklynx.tech>
2021-03-15 02:35:38 +0000

    Changes to pi code to add screen/sensor libraries

--- src/piCoprocessor/rpicode.py
+++ src/piCoprocessor/rpicode.py
@@ -1,19 +1,15 @@
-import os, sys, digitalio, board, busio
+import os, sys, digitalio, board, busio, time
  
 from adafruit_rgb_display.rgb import color565
 import adafruit_rgb_display.st7789 as st7789
 from adafruit_lc709203f import LC709203F
 import adafruit_bmp3xx
-
+from PIL import Image, ImageDraw, ImageFont
 
 
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
-
-
-
-
 # Create sensor objects
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 battSensor = LC709203F(i2c)
@@ -26,7 +22,7 @@ cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = None
 BAUDRATE = 64000000
-display = st7789.ST7789(board.SPI(), cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE, width=135, height=240, x_offset=53, y_offset=40)
+disp = st7789.ST7789(board.SPI(), cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE, width=135, height=240, x_offset=53, y_offset=40)
 height = disp.width
 width = disp.height
 image = Image.new("RGB", (width, height))
@@ -45,20 +41,28 @@ backlight.value = True
 
 while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    ip = 'cock'
-    CPU = 'and'
-    Disk = 'ball'
-    Temp = 'torture'
+    GPS = 'On the way'
+    try:
+        Battery = "Battery: %0.3f" % (battSensor.cell_voltage)
+    except:
+        Battery = "error"
+    try:
+        Voltage = str(type(battSensor.cell_percent))
+    except:
+        Voltage = "error"
+
+    Temp = "Pressure: {:6.1f}".format(bmp.pressure)
+    Pressure = "Temperature: {:5.2f}".format(bmp.temperature)
     y = top
-    draw.text((x, y), IP, font=font, fill="#FFFFFF")
-    y += font.getsize(IP)[1]
-    draw.text((x, y), CPU, font=font, fill="#FFFF00")
-    y += font.getsize(CPU)[1]
-    draw.text((x, y), MemUsage, font=font, fill="#00FF00")
-    y += font.getsize(MemUsage)[1]
-    draw.text((x, y), Disk, font=font, fill="#0000FF")
-    y += font.getsize(Disk)[1]
-    draw.text((x, y), Temp, font=font, fill="#FF00FF")
+    draw.text((x, y), GPS, font=font, fill="#FFFFFF")
+    y += font.getsize(GPS)[1]
+    draw.text((x, y), Battery, font=font, fill="#FFFF00")
+    y += font.getsize(Battery)[1]
+    draw.text((x, y), Voltage, font=font, fill="#FFFF00")
+    y += font.getsize(Voltage)[1]
+    draw.text((x, y), Temp, font=font, fill="#00FF00")
+    y += font.getsize(Temp)[1]
+    draw.text((x, y), Pressure, font=font, fill="#0000FF")
  
     # Display image.
     disp.image(image, rotation)
